3afe15bf62e880352da1946b692970c465f4b142
john lopokoiyit <ljptios@gmail.com>
2017-04-27 11:29:40 +0300

    changed css from class to id

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 
      jQuery(document).ready(function() {
             jQuery(".clickable").click(function() {
-              jQuery(".walrus-showing").toggle();
-              jQuery(".walrus-hidden").toggle();
+              jQuery("#walrus-showing").toggle();
+              jQuery("#walrus-hidden").toggle();
             });
           });
